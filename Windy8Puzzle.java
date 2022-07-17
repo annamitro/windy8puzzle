b04cbf532aafd4aab6a500a9c021881f9eae114c
annamitro <54995491+annamitro@users.noreply.github.com>
2022-07-16 22:58:04 -0400

    Add files via upload

---
+++ Windy8Puzzle.java
@@ -0,0 +1,24 @@
+package com.company;
+
+
+public class Windy8Puzzle {
+    private int[][] board;
+
+    public Windy8Puzzle() {
+        // row 0
+        board[0][0] = 1;
+        board[0][1] = 5;
+        board[0][2] = 0;
+
+        // row 1
+        board[1][0] = 6;
+        board[1][1] = 7;
+        board[1][2] = 4;
+
+        // row 2
+        board[2][0] = 6;
+        board[2][1] = 7;
+        board[2][2] = 4;
+
+    }
+}
